a7d67780a4484470b91eb7cbc9a8a320a1060055
Metis1589 <Metis1589@gmail.com>
2013-06-04 22:43:24 +0300

    Status added

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of contact
+ *
+ * @author Metis
+ */
+class contact {
+    //put your code here
+}
+
+?>
